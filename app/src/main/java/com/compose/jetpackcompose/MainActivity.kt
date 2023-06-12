26e522fcb412c4f5505d53d36c93ddc7b05f500a
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-12 22:10:19 +0900

    Spacer

--- app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
@@ -3,65 +3,48 @@ package com.compose.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
-import com.compose.jetpackcompose.ui.theme.Purple40
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SignInScreenPreview()
+            LikeButtonPreview()
         }
     }
 }
 
 @Composable
-fun SignInScreen() {
-    val loginTextStyle = androidx.compose.ui.text.TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Bold,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp,
-        color = Color.Black
-    )
-
-    Column(
-        verticalArrangement = Arrangement.SpaceEvenly,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
+fun LikeButton() {
+    Button(
+        onClick = {},
+        contentPadding = PaddingValues(start = 20.dp, top = 12.dp, end = 20.dp, bottom = 12.dp)
     ) {
-        Text(
-            text = "One Market",
-            style = loginTextStyle
+        Icon(
+            Icons.Filled.Favorite,
+            contentDescription = "Favorite",
+            modifier = Modifier.size(ButtonDefaults.IconSize)
         )
-        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Purple40)) {
-            Text(text = "로그인", style = loginTextStyle)
-        }
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+        Text(text = "Like", color = Color.White)
     }
 }
 
-@Preview("default", "round")
+@Preview
 @Composable
-fun SignInScreenPreview() {
-    JetpackComposeTheme {
-        SignInScreen()
-    }
+fun LikeButtonPreview() {
+    LikeButton()
 }
