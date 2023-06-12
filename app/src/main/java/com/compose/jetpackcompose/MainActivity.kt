2707a0533a30c9ecd45501a24bc1a4816e3d480b
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-12 23:15:09 +0900

    #2 Top app bar
    Scaffold를 활용하여 Top app bar 구현

--- app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.compose.jetpackcompose
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
@@ -22,44 +23,49 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ConfirmDialogPreview()
+            PaymentScreenPreview()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ConfirmDialog() {
-    AlertDialog(onDismissRequest = { /*TODO*/ },
-        confirmButton = {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "확인")
-            }
-        },
-        dismissButton = {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "취소")
-            }
-        },
-        title = {
-            Row{
-                Icon(
-                    imageVector = Icons.Default.Face,
-                    contentDescription = null,
-                    modifier = Modifier.align(Alignment.CenterVertically)
-                )
-                Spacer(Modifier.size(10.dp))
-                Text(text = "제목")
-            }
-        },
-        text = { Text(text = "내용") }
+fun PaymentScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "결제 수단 등록") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Outlined.ArrowBack, contentDescription = null
+                        )
+                    }
+                })
+        }
     )
+    { paddingValues ->
+        Box(modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize()) {
+            Icon(
+                imageVector = Icons.Filled.CheckCircle,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(Alignment.TopCenter),
+                tint = Color.Blue
+            )
+        }
+    }
 }
 
-@Preview("default", "dialog")
-@Preview("dark theme", "dialog", uiMode = UI_MODE_NIGHT_YES)
+
+@Preview
 @Composable
-fun ConfirmDialogPreview() {
-    JetpackComposeTheme(dynamicColor = false) {
-        ConfirmDialog()
+fun PaymentScreenPreview() {
+    JetpackComposeTheme {
+        PaymentScreen()
     }
 }
