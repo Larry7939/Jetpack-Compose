35a6958eb1d44829b579339022a9d68410681f2f
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-12 23:00:14 +0900

    #1 다이얼로그 구현

--- app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
@@ -1,50 +1,65 @@
 package com.compose.jetpackcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LikeButtonPreview()
+            ConfirmDialogPreview()
         }
     }
 }
 
 @Composable
-fun LikeButton() {
-    Button(
-        onClick = {},
-        contentPadding = PaddingValues(start = 20.dp, top = 12.dp, end = 20.dp, bottom = 12.dp)
-    ) {
-        Icon(
-            Icons.Filled.Favorite,
-            contentDescription = "Favorite",
-            modifier = Modifier.size(ButtonDefaults.IconSize)
-        )
-        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
-        Text(text = "Like", color = Color.White)
-    }
+fun ConfirmDialog() {
+    AlertDialog(onDismissRequest = { /*TODO*/ },
+        confirmButton = {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "확인")
+            }
+        },
+        dismissButton = {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "취소")
+            }
+        },
+        title = {
+            Row{
+                Icon(
+                    imageVector = Icons.Default.Face,
+                    contentDescription = null,
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+                Spacer(Modifier.size(10.dp))
+                Text(text = "제목")
+            }
+        },
+        text = { Text(text = "내용") }
+    )
 }
 
-@Preview
+@Preview("default", "dialog")
+@Preview("dark theme", "dialog", uiMode = UI_MODE_NIGHT_YES)
 @Composable
-fun LikeButtonPreview() {
-    LikeButton()
+fun ConfirmDialogPreview() {
+    JetpackComposeTheme(dynamicColor = false) {
+        ConfirmDialog()
+    }
 }
