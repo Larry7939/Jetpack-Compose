b6c188de82379de553958414aa33d5153d38a54a
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-06-12 19:20:49 +0900

    #2 레이아웃 구현

--- app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/compose/jetpackcompose/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -14,7 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.compose.jetpackcompose.ui.theme.JetpackComposeTheme
 import com.compose.jetpackcompose.ui.theme.Purple40
 
@@ -22,41 +24,36 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            UserGuidePopupPreview()
+            SignInScreenPreview()
         }
     }
 }
-@Composable
-fun UserGuidePopup() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight(),
-        verticalAlignment = Alignment.CenterVertically,
-    ) {
-        Button(
-            onClick = { /*TODO*/ }, modifier = Modifier
-                .weight(1f)
-                .background(Color.White),
-            colors = ButtonDefaults.buttonColors(Purple40)
-        ) {
 
-            Text(text = "취소")
-        }
-        Button(
-            onClick = { /*TODO*/ }, modifier = Modifier
-                .weight(2f)
-                .background(Color.White),
-            colors = ButtonDefaults.buttonColors(Purple40)
-        ) {
-            Text(text = "확인")
+@Composable
+fun SignInScreen() {
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Text(
+            text = "One Market",
+            fontSize= 25.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+        Button(onClick = { /*TODO*/ }, colors = ButtonDefaults.buttonColors(Purple40)) {
+            Text(text = "로그인")
         }
     }
 }
+
 @Preview("default", "round")
 @Composable
-fun UserGuidePopupPreview() {
+fun SignInScreenPreview() {
     JetpackComposeTheme {
-        UserGuidePopup()
+        SignInScreen()
     }
 }
